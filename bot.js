66c4b7073182dfcd7134e57101e02b1f961d5119
Abdul-Abdi <46908625+Abdul-Abdi@users.noreply.github.com>
2019-07-27 21:01:51 -0400

    initial

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+// Configure logger settings
+client.on('ready', () => {
+    console.log('Logged in as popchatbot!');
+});
+
+client.login(auth.token);
+
+const friv = new Discord.Attachment('https://cdn.discordapp.com/attachments/567392980525121565/601581914461175809/Screenshot_20190717-235040.jpg')
+const messy = new Discord.Attachment('https://i.imgur.com/fEKbH8i.gif')
+const jess = new Discord.Attachment('https://cdn.discordapp.com/attachments/604571326794956802/604596555458281472/ezgif.com-resize.gif')
+
+client.on('message', msg => {
+
+  if (msg.content.substring(0, 2) == 'p!') {
+
+  var args = msg.content.substring(2).split(' ');
+
+  switch(args[0]) {
+    case 'friv': 
+        msg.channel.send(friv);
+    break; 
+    case 'messy':
+        msg.channel.send(messy);
+    break; 
+    case 'jess':
+        msg.channel.send(jess);
+    break; 
+    default:
+        msg.reply('eat a dick');
+    break; 
+        
+  }
+}
+});
